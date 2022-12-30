1af7d0537625c51c9969a8c067210efe34fd5e1a
LukasWalker1 <lukasogwalker@gmail.com>
2022-12-30 01:38:00 +0100

    removed two content files

--- content/render_template.py
+++
@@ -1,5 +0,0 @@
-from server.webserver import register, MIME_TYPE, render_page
-
-@register(route='/',type=MIME_TYPE.HTML)
-def main_page():
-    return render_page('/content/index.html',{'test':'5'})
